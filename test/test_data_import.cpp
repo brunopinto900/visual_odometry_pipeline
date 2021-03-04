a3b1b25c9551ca82749c0871d0ee6415decc8eca
brunopinto900 <bruno@DESKTOP-TSTEU1E.localdomain>
2021-03-04 17:45:08 +0000

    new: Created video from a sequence of images (for testing)

--- test/test_data_import.cpp
+++ test/test_data_import.cpp
@@ -5,14 +5,19 @@ int main(int argc, char *argv[])
 {
     DataImport data_import;
 
+    int frames = 0;
     /*check the results*/
     cout << WHITE"Number of images = " << data_import.camera_data.size() << endl;
     cout << WHITE"Number of IMU measurements = " << data_import.imu_data.size() << endl;
 
-    cout << data_import.camera_data[0].img_name;
     Mat img = imread(data_import.camera_data[0].img_name, IMREAD_GRAYSCALE );
 
-    imshow("test",img);
-    waitKey();
+     for( Camera cam : data_import.camera_data)
+    {
+        Mat img = imread(cam.img_name, IMREAD_GRAYSCALE );
+        imshow("test",img);
+        waitKey(50);
+        frames++;
+    }
     return 0;
 }
